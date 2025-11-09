1815334cbb038440a9e1f313fed9bdd07873d00b
arcadeflix <163352231+arcadeflix@users.noreply.github.com>
2025-11-09 20:33:00 -0300

    Add files via upload

---
+++ ad1.js
@@ -0,0 +1,9 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8676088752361266"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:300px"
+     data-ad-client="ca-pub-8676088752361266"
+     data-ad-slot="2132317279"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
